6833becdff55ce2645027f5914fa7198fa05354a
LYH <334835178@qq.com>
2019-01-17 16:55:16 +0800

    完成任务4：调整算法，执行10次，pagerank值稳定

--- src/main/java/com/neuedu/FourStepMapper.java
+++ src/main/java/com/neuedu/FourStepMapper.java
@@ -18,14 +18,12 @@ public class FourStepMapper extends Mapper<LongWritable,Text,Text,Text> {
     杨宾 0.1*0.286
     胡斐 0.1*0.286
     中年武师	#何思豪:0.071;大汉:0.071;孙伏虎:0.214;尉迟连:0.071;杨宾:0.286;胡斐:0.286*/
-
+//第二次运行 输入：中年武师	0.0331#何思豪:0.071;大汉:0.071;孙伏虎:0.214;尉迟连:0.071;杨宾:0.286;胡斐:0.286
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] split = value.toString().split("\t");//0:中年武师 1:0.1#何思豪:0.071;大汉:0.071;孙伏虎:0.214;尉迟连:0.071;杨宾:0.286;胡斐:0.286
-        //String str=split[0]+"\t"+split[1].substring(3);//中年武师	#何思豪:0.071;大汉:0.071;孙伏虎:0.214;尉迟连:0.071;杨宾:0.286;胡斐:0.286
-        context.write(new Text(split[0]),new Text(split[1].substring(3)));
-
         String[] split1 = split[1].split("#");//0:0.1    1:何思豪:0.071;大汉:0.071;孙伏虎:0.214;尉迟连:0.071;杨宾:0.286;胡斐:0.286
+        context.write(new Text(split[0]),new Text("#"+split1[1]));
         double quanzhong=Double.parseDouble(split1[0]);//0.1
         String[] split2 = split1[1].split(";");//何思豪:0.071  大汉:0.071  孙伏虎:0.214  尉迟连:0.071  杨宾:0.286  胡斐:0.286
         for(String s1:split2){
